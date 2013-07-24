43545c9a2ca1e8dadbb2bc60d9816a5a57a82292
Alain Dejoux <adejoux@krystalia.net>
2013-07-24 15:16:50 +0200

    report mode on multiple snap

--- bin/aix_snap_parser.rb
+++ bin/aix_snap_parser.rb
@@ -21,12 +21,17 @@ version=0.4
 
 snap_file=""
 config_file=""
+snap_dir=""
 
 opt_parser = OptionParser.new do |opts|
   opts.banner = "Usage: analyse_snap.rb [-f snap_file] [-c config_file] [-h] [-v]"
 
   opts.on('-f snap', '--file=snap', 'snap file') do |v|
     snap_file = v
+  end
+
+  opts.on('-d snap_directory', '--directory=snap', 'snap directory') do |v|
+    snap_dir = v
   end
 
   opts.on('-c config_file', '--config_file=config_file', 'yaml config file') do |v|
@@ -44,7 +49,7 @@ opt_parser = OptionParser.new do |opts|
 end
 opt_parser.parse!
 
-unless File.file?(snap_file)
+unless File.file?(snap_file) or File.directory?(snap_dir)
   puts "Usage: analyse_snap.rb [-f snap_file] [-c config_file] [-h] [-v]"
   puts "error: file #{snap_file} is not readable"
   exit 1
@@ -61,110 +66,84 @@ end
 spreadsheet=Spreadsheet.new
 
 #create sheet for each file in config
-pc.sheets.each do |sheet|
-  spreadsheet.add_sheet(sheet)
-end
+spreadsheet.add_sheet('network')
 
-Gem::Package::TarReader.new(File.open(snap_file)).each do |entry|
 
-  pc.each_full_file do |file|
-    if entry.full_name.match(/#{file.name}/)
-      puts "Processing file : #{entry.full_name}"
-      spreadsheet.current_sheet=file.sheet
-      spreadsheet.add_summary File.basename(entry.full_name)
-      entry.read.split("\n").each do |line|
-        if file.skip_line and line.match(/#{file.skip_line}/)
-          next
-        end
-        if file.skip_empty and line.chomp.empty?
-          next
-        end
+spreadsheet.current_sheet="network"
+spreadsheet.set_table
+spreadsheet.add_row ['partition',
+                     'model',
+                     'serial',
+                     'adapter',
+                     'slot',
+                     'description',
+                     'mac address',
+                     'mtu',
+                     'ip',
+                     'selected speed',
+                     'running speed'
+                   ]
 
-        if file.separator.nil?
-          spreadsheet.add_row [line]
-        else
-          spreadsheet.add_row line.split(file.separator, file.sep_num)
+adapters=Hash.new{|hash, key| hash[key] = Hash.new}
+system={}
+
+snap_list=Dir.entries(snap_dir).select {|entry| entry.match(/snap.pax$/) }
+snap_list.each do |snap|
+  puts "SNAP FILE: snap"
+  snap_file=File.join(snap_dir, snap)
+  Gem::Package::TarReader.new(File.open(snap_file)).each do |entry|
+    pc.each_matched_file do |file|
+      if entry.full_name.match(/#{file.name}/)
+        current_adapter=""
+        media_adapter=""
+        puts "Processing file : #{entry.full_name}"
+        next if file.is_excluded? entry.full_name
+
+        entry.read.split("\n").each do |line|
+          file.each_matchexp do |matchexp|
+            matchres=line.match(matchexp.name)
+            unless matchres.nil?
+              case matchexp.label
+                when "adapter" then current_adapter=matchres[1]
+                when "slot" then adapters[current_adapter]["slot"]=matchres[1]
+                when 'description' then adapters[current_adapter]["description"]=matchres[1]
+                when 'mac' then adapters[current_adapter]["mac"]=matchres[1]
+                when 'serial' then system["serial"]=matchres[1]
+                when 'model' then system["model"]=matchres[1]
+                when 'media_adapter' then media_adapter=matchres[1]
+                when 'media_selected' then adapters[media_adapter]['media_selected']=matchres[1]
+                when 'media_running' then adapters[media_adapter]['media_running']=matchres[1]
+                when 'interface'
+                  adapter=matchres[1].sub(/en/, 'ent')
+                  adapters[adapter]['mtu']=matchres[2]
+                  adapters[adapter]['ip']=matchres[3]
+              end
+            end
+          end
+        entry.rewind
         end
       end
-      entry.rewind
     end
   end
-
-  pc.each_matched_file do |file|
-    if entry.full_name.match(/#{file.name}/)
-      puts "Processing file : #{entry.full_name}"
-      next if file.is_excluded? entry.full_name
-
-      spreadsheet.current_sheet=file.sheet
-      spreadsheet.add_summary File.basename(entry.full_name)
-      spreadsheet.set_table
-
-      entry.read.split("\n").each do |line|
-        file.each_matchexp do |matchexp|
-          matchres=line.match(matchexp.name)
-          unless matchres.nil?
-            if file.header?
-              spreadsheet.add_table_header matchexp.label
-            end
-
-            spreadsheet.add_table_body matchres[1]
-            if matchexp.new_row?
-              spreadsheet.add_body_row
-              file.no_header!
-            end
-          end
-        end
-      end
-      entry.rewind
-    end
-  end
-
-  pc.each_parsed_file do |file|
-    if entry.full_name.match(/#{file.name}/)
-      puts "Processing file : #{entry.full_name}"
-      sp=SnapParser.new
-      entry.read.split("\n").each do |line|
-        if line.match(/^\.\.\.\.\./) and sp.previous_line.match(/^\.\.\.\.\./)
-            sp.header=false
-        end
-
-        pattern_matched=false
-        file.each_pattern do |pattern|
-          if line.match(/^.....    #{pattern.name}/)
-            sp.pattern=pattern
-            spreadsheet.current_sheet=sp.pattern.sheet
-            spreadsheet.add_summary line.sub(/^.....    /, '')
-            pattern_matched=true
-            break
-          end
-        end
-
-        next if pattern_matched
-
-        if sp.header
-          if line.match(/^\.\.\.\.\./)
-            sp.previous_line = line
-            next
-          end
-
-          unless line.chomp.empty?
-            if sp.pattern.skip_line and line.match(/#{sp.pattern.skip_line}/)
-              next
-            end
-            if sp.pattern.separator.nil?
-              spreadsheet.add_row [line]
-            else
-              spreadsheet.add_row line.split(sp.pattern.separator, sp.pattern.sep_num)
-            end
-          end
-        end
-        sp.previous_line = line
-      end
-      entry.rewind
-    end
+  spreadsheet.current_sheet='network'
+  server=File.basename(snap_file).sub(/_.*/, '')
+  adapters.each_key do |adapter|
+    spreadsheet.add_row [ server,
+                          system["model"],
+                          system["serial"],
+                          adapter,
+                          adapters[adapter]["slot"],
+                          adapters[adapter]["description"],
+                          adapters[adapter]["mac"],
+                          adapters[adapter]['mtu'] || "N/A",
+                          adapters[adapter]['ip']  || "N/A",
+                          adapters[adapter]['media_selected'],
+                          adapters[adapter]['media_running']
+                           ]
   end
 end
 
-output_file=File.basename(snap_file).sub(/_.*/, '')
-spreadsheet.save "#{output_file}.xlsx"
-puts "Output file generated : #{output_file}.xlsx"
+#puts adapters.inspect
+#puts system.inspect
+spreadsheet.save "report.xlsx"
+puts "Output file generated : report.xlsx"
